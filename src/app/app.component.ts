24cff42b0069e487d20b7fb40d888ada93c29373
Adrian Faciu <adrian.faciu@gmail.com>
2017-08-26 22:45:47 +0300

    Refactored main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,9 @@
 import { Component } from '@angular/core';
-import { Store, Action } from '@ngrx/store';
-import { AppState } from './store/app.reducer';
-import { INCREMENT_ACTION } from './store/app.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
-
-  constructor(private store: Store<AppState>) { }
-
-  onIncrement() {
-    this.store.dispatch({ type: INCREMENT_ACTION });
-  }
 }
