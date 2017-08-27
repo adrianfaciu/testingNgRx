8c21c30f940edd756dae8bd1149de2791d989622
Adrian Faciu <adrian.faciu@gmail.com>
2017-08-27 18:33:25 +0300

    Working tests

--- src/app/store/app.time-effect.spec.ts
+++ src/app/store/app.time-effect.spec.ts
@@ -1,33 +1,12 @@
 import { Actions } from '@ngrx/effects';
-import { cold, hot, getTestScheduler } from 'jasmine-marbles';
+import { cold, getTestScheduler } from 'jasmine-marbles';
 import { Observable } from 'rxjs/Observable';
-
-import { fakeAsync, tick } from '@angular/core/testing';
 
 import * as actions from './app.actions';
 import { AppTimeEffect } from './app.time-effect';
 
 describe('app time effects', () => {
-  xit('can fetch data with debounce', () => {
-    const source = hot('a', { a: { type: actions.DATA_WITH_DEBOUNCE_FETCH } });
-
-    const effect = new AppTimeEffect(new Actions(source));
-
-    const expected = cold('a', { a: { type: actions.DATA_WITH_DEBOUNCE_FETCH_SUCCESS } });
-    expect(effect.fetchDataWithDebounce$).toBeObservable(expected);
-  });
-
-  xit('can fetch data with debounce (fakeAsync)', fakeAsync(() => {
-    const source = cold('a', { a: { type: actions.DATA_WITH_DEBOUNCE_FETCH } });
-
-    const effect = new AppTimeEffect(new Actions(source));
-
-    const expected = cold('a', { a: { type: actions.DATA_WITH_DEBOUNCE_FETCH_SUCCESS } });
-    tick(200);
-    expect(effect.fetchDataWithDebounce$).toBeObservable(expected);
-  }));
-
-  xit('can fetch data with debounce (mocking debounce)', () => {
+  it('can fetch data with debounce (mocking debounce)', () => {
     const source = cold('a', { a: { type: actions.DATA_WITH_DEBOUNCE_FETCH } });
 
     mockObservable();
@@ -37,14 +16,15 @@ describe('app time effects', () => {
     expect(effect.fetchDataWithDebounce$).toBeObservable(expected);
   });
 
-  xit('can fetch data with debounce (testScheduler)', () => {
-    const scheduler = getTestScheduler();
-    const source = scheduler.createHotObservable('a', { a: { type: actions.DATA_WITH_DEBOUNCE_FETCH } });
+  it('can fetch data with debounce (test scheduler)', () => {
+    const source = cold('a', { a: { type: actions.DATA_WITH_DEBOUNCE_FETCH } });
 
+    mockObservableWithScheduler(getTestScheduler());
     const effect = new AppTimeEffect(new Actions(source));
 
-    scheduler.expectObservable(effect.fetchDataWithDebounce$).toBe('a', { a: { type: actions.DATA_WITH_DEBOUNCE_FETCH_SUCCESS } });
-    scheduler.flush();
+    // - is a 10 frame interval, since we use 100 in the effect we use 10 - characters
+    const expected = cold('----------a', { a: { type: actions.DATA_WITH_DEBOUNCE_FETCH_SUCCESS } });
+    expect(effect.fetchDataWithDebounce$).toBeObservable(expected);
   });
 
   function mockObservable() {
